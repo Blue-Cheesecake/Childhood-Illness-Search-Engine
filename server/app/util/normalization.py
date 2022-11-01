17e097c4f6608cefaf18fd4c150edeb55cc1f600
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-11-01 13:32:48 +0700

    feat: import csv data

--- server/app/util/normalization.py
+++ server/app/util/normalization.py
@@ -1,6 +1,10 @@
 from typing import Dict
+import pandas as pd
+import os
 
-FILE_PATH = 'app/data/test.csv'
+# There will be many csv file in data dir
+# The FILE_PATH will be folder path. Not only one csv file
+DATA_DIR_PATH = 'app/data'
 
 
 def getNormalizedData() -> Dict:
@@ -10,5 +14,19 @@ def getNormalizedData() -> Dict:
       Dict: Data that's ready to be digested in Elastic
   """
   # TODO: Implements import, and normalization data
+  files = os.listdir(DATA_DIR_PATH)
+
+  if files == None:
+    raise Exception("Something is wrong. Files is null")
+  if len(files) == 0:
+    raise Exception("There is no file in data folder")
+
+  numsFiles = len(files)
+
+  for csvFile in files:
+
+    FILE_PATH = f"{DATA_DIR_PATH}/{csvFile}"
+
+    pf = pd.read_csv(FILE_PATH)
 
   return {'final': 'dictionary'}
