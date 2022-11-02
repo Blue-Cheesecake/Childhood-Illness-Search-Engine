589e306b91f3e7c990fe4d31017afd7a7521c7b6
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-11-02 21:50:09 +0700

    feat: starter code

---
+++ client/lib/data/remote/network/api_end_point.dart
@@ -0,0 +1,7 @@
+class ApiEndPoints {
+  // /illness/<string:qname> get by name
+  final String getIllness = "/illness/";
+
+  // illnessList/<string:qSymptoms> get by symptoms
+  final String getIllnessList = "illnessList/>";
+}
