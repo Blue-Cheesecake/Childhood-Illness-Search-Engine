882e4864ec4aaa5baa187d9e07c9c2aa821e2bdd
Nipphit Apisitpuwakul <nipphit.api@student.mahidol.ac.th>
2022-11-14 19:39:32 +0700

    feat: experiment illnesslist API

--- server/app/resources/IllnessList.py
+++ server/app/resources/IllnessList.py
@@ -1,9 +1,10 @@
-from typing import Dict
+from typing import Dict, List
 
 from elastic import elastic_client
 from flask_restful import Resource
 
 from models.IllnessesList import IllnessList as IllnessListModel
+from models.Illness import Illness as IllnessModel
 
 
 class IllnessList(Resource):
@@ -20,6 +21,24 @@ class IllnessList(Resource):
         Dict: Json Format (Dictionary)
     """
 
-    data = []
+    data: List[IllnessModel] = []
+    resp = elastic_client.search(index="test_s1", query={
+    "term": {
+      "symptoms": qSymptoms
+    }
+  })
+    for hit in resp['hits']['hits']:
+      source = hit["_source"]
+      illnessModel = IllnessModel(
+        name=source['name'],
+        description=source['description'],
+        symptoms=source['symptoms'],
+        treating=source['treating'],
+        preventing=source['causes/preventing'],
+        common=source['common state'],
+        link=source['link']
+      )
+      data.append(illnessModel)
+    
 
     return IllnessListModel(illnessArray=data).json()
