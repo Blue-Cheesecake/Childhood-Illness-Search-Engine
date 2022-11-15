49ce7b4723471ae40944092b053e272cf170464f
Nipphit Apisitpuwakul <nipphit.api@student.mahidol.ac.th>
2022-11-15 22:21:20 +0700

    add: query + q_normalization

--- server/app/resources/IllnessList.py
+++ server/app/resources/IllnessList.py
@@ -7,6 +7,19 @@ from models.IllnessesList import IllnessList as IllnessListModel
 from models.Illness import Illness as IllnessModel
 
 
+#Normalization
+import pandas as pd
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+porter = PorterStemmer()
+import sys  
+import contractions
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+
 class IllnessList(Resource):
   """IllnessList Resource"""
 
@@ -21,10 +34,33 @@ class IllnessList(Resource):
         Dict: Json Format (Dictionary)
     """
 
+    words = word_tokenize(qSymptoms)
+
+    for i in range(len(words)):
+      words[i] = porter.stem(words[i])
+
+    for i in range(len(words)):
+      words[i] = lemmatizer.lemmatize(words[i], pos ="a")
+
+    for i in range(len(words)):
+      words[i] = contractions.fix(words[i])
+    
+    qSymptoms_n = ''
+    for word in words:
+      qSymptoms_n = qSymptoms_n+' '+word
+    qSymptoms_n = qSymptoms_n[1:]
+
+
     data: List[IllnessModel] = []
     resp = elastic_client.search(index="test_s1", query={
-    "term": {
-      "symptoms": qSymptoms
+    "match": {
+        "symptoms_n": 
+        {
+          "query":qSymptoms_n,
+          "fuzziness": "auto",
+          "max_expansions": 1,
+          "fuzzy_transpositions": "true"
+        }
     }
   })
     for hit in resp['hits']['hits']:
